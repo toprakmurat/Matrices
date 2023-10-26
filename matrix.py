0cc87fd24491b062c3e980addbee0c130e966021
itu-itis23-toprakm23 <toprakm23@itu.edu.tr>
2023-10-26 21:17:24 +0300

    add gauss-jordan method

--- matrix.py
+++ matrix.py
@@ -185,14 +185,14 @@ class Matrix:
 
     # Apply Gaussian elimination for square matrices
     @staticmethod
-    def gaussian_elimination(matrix):
+    def gaussian_elimination(matrix, augmenting_matrix):
         if check_matrix(matrix, is_square=True) is False:
             raise Exception('Invalid Matrix')
 
         for row_index, row in enumerate(matrix):
             if row_index == len(matrix) - 1:
                 # elementary operations for the last row is not needed
-                return matrix
+                return matrix, augmenting_matrix
 
             pivot = row[row_index]
 
@@ -212,13 +212,41 @@ class Matrix:
                 # If alpha equals to 0, then no operation is needed. Just pass that row.
                 if alpha == 0:
                     continue
-                matrix[current_ind] = [val * alpha + row_below[val_index]
-                                       for val_index, val in enumerate(row)]
+                matrix[current_ind] = [val * alpha + row_below[ind]
+                                       for ind, val in enumerate(row)]
+                augmenting_matrix[current_ind] = [val * alpha + augmenting_matrix[current_ind][ind]
+                                            for ind, val in enumerate(augmenting_matrix[row_index])]
 
+    @staticmethod
+    def gauss_jordan(matrix):
+        if check_matrix(matrix, is_square=True) is False:
+            raise Exception('Invalid Matrix')
 
-# TODO: GAUSS-JORDAN METHOD
+        # Create an identity matrix for augmented matrix
+        identity_matrix = Matrix.create_diagonal_matrix(len(matrix), value=1)
+        upper_matrix, augmenting_matrix = Matrix.gaussian_elimination(matrix, identity_matrix)
+        for i, row in enumerate(reversed(upper_matrix)):
+            pivot_index = (-1 * i) - 1
+            if i == 0:
+                pivot_index = -1
 
-a = [[1, 1, 2],
-     [-1, -1, 1],
-     [3, 1, 1]]
-print(Matrix.gaussian_elimination(a))
+            pivot = row[pivot_index]
+
+            # No temporary fails because we fixed them in gaussian elimination
+            if pivot == 0:
+                continue
+
+            for index_upper, row_upper in enumerate(matrix[:pivot_index]):
+                alpha = -1 * row_upper[pivot_index] / pivot
+                matrix[index_upper] = [val * alpha + row_upper[ind]
+                                       for ind, val in enumerate(row)]
+
+                row_index = len(matrix) - i - 1  # row is started as reversed
+                augmenting_matrix[index_upper] = [val * alpha + augmenting_matrix[index_upper][ind]
+                                            for ind, val in enumerate(augmenting_matrix[row_index])]
+                # As for equality in indexes
+                # augmenting_matrix[index_upper] = matrix[index_upper]
+                # augmenting_matrix[row_index] = row
+                # Those are same indexes respectively
+
+        return matrix, augmenting_matrix
